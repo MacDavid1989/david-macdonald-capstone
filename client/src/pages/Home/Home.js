37654557e5402a795bb5252003cb8fad9bf37930
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-02 21:48:45 -0500

    Create Client structure. Add components, pages, scss and utils folders. Setup environment variables file and install dotenv. Remove create-react-app template data. Set up Home component and index default import/export. Import Home into App. Set boundary reset styling on body element through index.scss. Add personal icon to public file. Create typography, variables and mixins scss partial files.

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../../scss/Home.scss';
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Welcome to Simplify
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default home;
