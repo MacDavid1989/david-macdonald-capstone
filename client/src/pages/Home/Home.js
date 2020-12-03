a9b79c68db7cfbc0de3a18ee87b90badc5b5b502
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-03 13:12:14 -0500

    Create minimalist form and connect onChange handlers. Add state to component. Utilize componentDidMount to reset state for the time being. Add form handler in the event the form expands from two inputs. Add select options for the meal types available for this app.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
 import '../../scss/Home.scss';
+import axios from 'axios';
+
+// https://api.edamam.com/search?q=${query}+${YOUR_APP_ID}+${YOUR_APP_KEY}
+const API_URL = process.env.REACT_APP_API_URL;
+const API_ID = process.env.REACT_APP_API_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class home extends Component {
+    state = {
+        query: '',
+        mealType: ''
+    }
+
+    componentDidMount() {
+        this.setState({
+            query: '',
+            mealType: ''
+        })
+    }
+
+    handleMealSearch = (e) => {
+        this.setState({
+            query: e.target.query.value,
+            mealType: e.target.mealType.value
+        })
+        axios.get(API_URL+this.state.query+API_ID+API_KEY+this.state.mealType)
+    }
+
+    changeSearchIngredient = (e) => {
+        this.setState({
+            query: e.target.value,
+        }, ()=>console.log(this.state.query))
+    }
+
+    changeMealType = (e) => {
+        this.setState({
+            mealType: e.target.value
+        }, ()=>console.log(this.state.mealType))
+    }
+
     render() {
         return (
             <div>
                 <h1>
-                    Welcome to Simplify
+                    Welcome! Search for your meal!
                 </h1>
+                <form>
+                    <input type='search' name='searchIngredient' value={this.state.query} onChange={this.changeSearchIngredient}/>
+                    <select name='mealType' value={this.state.mealType} onChange={this.changeMealType}>
+                    <option defaultValue value="default" hidden>
+                      Please select a meal:
+                    </option>
+                    <option value="Breakfast">Breakfast</option>
+                    <option value="Lunch">Lunch</option>
+                    <option value="Dinner">Dinner</option>
+                    </select>
+                </form>
             </div>
         );
     }
