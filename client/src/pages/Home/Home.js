cc9aea97969d10ca0ecd30e6cd2fe75d1092709e
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-05 15:29:59 -0500

    Create previous and next button to GET the previous 10 meals or the next 10 meals.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,35 +3,25 @@ import '../../scss/Home.scss';
 import axios from 'axios';
 
 // https://api.edamam.com/search?q=${query}+${YOUR_APP_ID}+${YOUR_APP_KEY}
-const API_URL = process.env.REACT_APP_API_URL;
-const API_ID = process.env.REACT_APP_API_ID;
-const API_KEY = process.env.REACT_APP_API_KEY;
+
 
 class home extends Component {
     state = {
         query: '',
         mealType: '',
-        meals: ''
+        meals: '',
+        from: 0
     }
 
-    componentDidMount() {
-    }
+    getMeals = () => {
+        const API_URL = process.env.REACT_APP_API_URL;
+        const API_ID = process.env.REACT_APP_API_ID;
+        const API_KEY = process.env.REACT_APP_API_KEY;
+        const MEAL = `&mealType=${this.state.mealType}&from=${this.state.from}`;
+        const QUERY = this.state.query;
 
-    changeSearchIngredient = (e) => {
-        this.setState({
-            query: e.target.value.toLowerCase(),
-        }, ()=>console.log(this.state.query))
-    }
-
-    changeMealType = (e) => {
-        this.setState({
-            mealType: e.target.value
-        }, ()=>console.log(this.state.mealType))
-    }
-
-    componentDidUpdate() {
         this.state.query&&this.state.mealType&&!this.state.meals&&
-        axios.get(API_URL+this.state.query+API_ID+API_KEY+this.state.mealType)
+        axios.get(API_URL+QUERY+API_ID+API_KEY+MEAL)
         .then(response => {
             console.log(response)
             this.setState({
@@ -39,6 +29,33 @@ class home extends Component {
             })
             console.log(this.state.meals)
         }) 
+    }
+
+    changeSearchIngredient = (e) => {
+        this.setState({
+            query: e.target.value.toLowerCase(),
+        }, this.getMeals)   
+    }
+
+    changeMealType = (e) => {
+        this.setState({
+            mealType: e.target.value
+        }, this.getMeals)
+    }
+
+    handleNext = () => {
+        this.setState({
+            meals: '',
+            from: this.state.from + 10
+        }, this.getMeals)
+    }
+
+    handlePrevious = () => {
+        (this.state.from > 0)&&
+        this.setState({
+            meals: '',
+            from: this.state.from - 10
+        }, this.getMeals)
     }
 
     render() {
@@ -62,7 +79,7 @@ class home extends Component {
                     Select your meal!
                 </h1>
                 <ul>
-                    {/* {this.state.meals&&this.state.meals.map(meal =>
+                    {this.state.meals&&this.state.meals.map(meal =>
                     <li>
                         <div>
                             <img src={meal.image}/>
@@ -71,13 +88,15 @@ class home extends Component {
                                     {meal.label}
                                 </span>
                                 <span>
-                                    {`${Math.ceil(meal.calories)} cals`}
+                                    {`${Math.ceil(meal.calories / meal.yield)} cals`}
                                 </span>
                             </div>
                         </div>
                     </li>
-                    )} */}
+                    )}
                 </ul>
+                <button onClick={this.handlePrevious}>PREVIOUS</button>
+                <button onClick={this.handleNext}>NEXT</button>
             </div>
         );
     }
