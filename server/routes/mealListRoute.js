8683704bedb8819c181bf3ceb9f222e447f3a4b4
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-06 01:07:01 -0500

    Files in this commit are associated with the previous commit - see that message for details

--- server/routes/mealListRoute.js
+++ server/routes/mealListRoute.js
@@ -8,27 +8,28 @@ const ingredientsFile = './data/ingredients.json';
 // post route for meals to be added for user
 router.post('/', (req, res) => {
     const newMeal = req.body;
-    writeMeals(newMeal)
+    addNewMeal(newMeal)
     const newIngredients = newMeal.ingredients;
-    writeIngredients(newIngredients)
+    addNewIngredients(newIngredients)
     res.status(201).send(newMeal)
 })
 
 // get route for meals to be rendered on my meals page
-router.get('/', (req, res) => {
-    const mealList = getMeals()
-    res.status(200).json(mealList)
+router.get('/', (_req, res) => {
+    const meals = getMeals()
+    res.status(200).json(meals)
 })
 
 // delete route for removing meals from my meals page
 router.delete('/:id', (req, res) => {
-    console.log(req.params.id)
-    const mealList = getMeals()
-    mealList.map((meal,i)=>{
-        meal.id===req.params.id&&mealList.splice(i,1)&&
+    const ingredients = getIngredients().filter(ingredient => ingredient.mealId!==req.params.id)
+    writeIngredients(ingredients)
+    const meals = getMeals()
+    meals.forEach((meal,i) => {
+        meal.id===req.params.id&&meals.splice(i,1)&&
         res.status(202).json(meal)
     })
-    fs.writeFileSync(mealsFile, JSON.stringify([...mealList]), err=>console.log(err))
+    writeMeals(meals)
 })
 
 getMeals = () => {
@@ -39,16 +40,24 @@ getIngredients = () => {
     return JSON.parse(fs.readFileSync(ingredientsFile))
 }
 
-writeMeals = (newMeal) => {
-    const meals = getMeals()
-    meals.push(newMeal)
+writeMeals = (meals) => {
     fs.writeFileSync(mealsFile, JSON.stringify([...meals]), err=>console.log(err))
 }
 
-writeIngredients = (newIngredients) => {
-    const ingredients = getIngredients()
-    ingredients.push(newIngredients)
+writeIngredients = (ingredients) => {
     fs.writeFileSync(ingredientsFile, JSON.stringify([...ingredients]), err=>console.log(err))
+}
+
+addNewMeal = (newMeal) => {
+    const meals = getMeals()
+    meals.push(newMeal)
+    writeMeals(meals)
+}
+
+addNewIngredients = (newIngredients) => {
+    const ingredients = getIngredients()
+    ingredients.push(...newIngredients)
+    writeIngredients(ingredients)
 }
 
 module.exports = router;
