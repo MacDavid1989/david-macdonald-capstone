8cf22fa2f7217df104d8334724caedc5081528c1
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-09 11:52:45 -0500

    Remove state from Nav component and replace with all route links.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,82 +1,17 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../../scss/Home.scss';
+function Nav() {
 
-class Nav extends Component {
-    state = {
-        display: false,
-        firstLink: 'Search',
-        secondLink: 'Grocery List'
-    }
-
-    componentDidMount() {
-        this.props.location.pathname==='/'&&
-        this.setState({
-            display: false,
-            firstLink: 'Search',
-            secondLink: 'Grocery List'
-        });
-        
-        this.props.location.pathname==='/search'&&
-        this.setState({
-            display: true,
-            firstLink: 'My Meals',
-            secondLink: 'Grocery List'
-        });
-
-        this.props.location.pathname==='/my meals'&&
-        this.setState({
-            display: true,
-            firstLink: 'Search',
-            secondLink: 'Grocery List'
-        });
-
-        this.props.location.pathname==='/grocery list'&&
-        this.setState({
-            display: true,
-            firstLink: 'Search',
-            secondLink: 'My Meals'
-        });
-    }
-
-    componentDidUpdate() {
-        this.state.display===true&&this.props.location.pathname==='/'&&
-        this.setState({
-            display: false,
-            firstLink: 'Search'
-        });
-        
-        this.state.firstLink==='Search'&&this.props.location.pathname==='/search'&&
-        this.setState({
-            display: true,
-            firstLink: 'My Meals',
-            secondLink: 'Grocery List'
-        });
-
-        this.props.location.pathname==='/my meals'&&(this.state.firstLink==='My Meals'||this.state.secondLink==='My Meals')&&
-        this.setState({
-            display: true,
-            firstLink: 'Search',
-            secondLink: 'Grocery List'
-        });
-
-        this.state.secondLink==='Grocery List'&&this.props.location.pathname==='/grocery list'&&
-        this.setState({
-            display: true,
-            firstLink: 'Search',
-            secondLink: 'My Meals'
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <Link to="/">Hello</Link>
-                <Link to={`/${this.state.firstLink.toLowerCase()}`}>{this.state.firstLink}</Link>
-                <Link style={{ display: this.state.display ? "initial" : "none" }} to={`/${this.state.secondLink.toLowerCase()}`}>{this.state.secondLink}</Link>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <NavLink to="/">Hello</NavLink>
+            <NavLink to="/search">Meal Search</NavLink>
+            <NavLink to="/my meals">Meal Plan</NavLink>
+            <NavLink to="/grocery list">Grocery List</NavLink>
+        </div>
+    );
 }
 
-export default withRouter(Nav);
+
+export default Nav;
