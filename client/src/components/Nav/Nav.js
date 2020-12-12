a4536ecfff9d60a05a173e4c5ecd7befbf1bb95a
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-12 18:34:09 -0500

    Add logo to Nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import '../../scss/Nav.scss';
+import logo from '../../assets/icons/grubbery.svg'
 function Nav() {
 
     return (
         <>
             <div className="nav">
-                    <NavLink className="nav__logo" to="/">Logo</NavLink>
+                    <NavLink className="nav__logo" to="/"><img src={logo} alt="grubbery logo"/></NavLink>
                 <div className="nav__links">
                     <NavLink className="nav__links-text" to="/search">Browse Recipes</NavLink>
                     <NavLink className="nav__links-text--alt" to="/my meals">Menu</NavLink>
