00b581bf90c8d00fce830a987a5183e7d1d9086f
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-12 22:18:19 -0500

    Finalize Nav styling at desktop.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 import '../../scss/Nav.scss';
 import logo from '../../assets/icons/grubbery.svg'
+
 function Nav() {
 
     return (
         <>
             <nav className="nav">
                 <div className="nav__container">
-                        <NavLink className="nav__logo" to="/"><img src={logo} alt="grubbery logo"/></NavLink>
+                    <NavLink className="nav__logo" to="/">
+                        <img src={logo} alt="grubbery logo"/>
+                    </NavLink>
                     <div className="nav__links">
                         <NavLink className="nav__links-text" to="/search">Browse Recipes</NavLink>
                         <NavLink className="nav__links-text--alt" to="/my meals">Menu</NavLink>
