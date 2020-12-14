0346bd16cc4fcbac49e7d82520dbd7c0c3333ea7
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-14 12:40:21 -0500

    Banner at mobile complete for Browse page

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -200,7 +200,7 @@ class Search extends Component {
             <div className="search">
                 <div className="search__banner">
                     <h1 className="search__banner-title">Browse Grub</h1>
-                    <h2 className="search__banner-subtitle">1.2 million delicious recipes at your fingertips</h2>
+                    <h2 className="search__banner-subtitle">Search over 1.5 million delicious recipes</h2>
                     <form className="search__form">
                         <input className="search__form-input" type='text' name='searchIngredient' placeholder="Search Recipes..." value={this.state.query} onChange={this.changeSearchIngredient}/>
                         <select className="search__form-select" name='mealType' value={this.state.mealType} onChange={this.changeMealType}>
