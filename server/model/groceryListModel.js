954b03e693ba11def8a4b1779940b645bf04f49a
David <62397679+MacDavid1989@users.noreply.github.com>
2020-12-15 01:08:20 -0500

    Place sorting into functions and create consolidate ingredients function to simplify grocery list function

--- server/model/groceryListModel.js
+++ server/model/groceryListModel.js
@@ -6,7 +6,86 @@ const groceriesFile = './data/groceries.json';
 const userItemsFile = './data/userItems.json';
 
 groceryList = (ingredients) => {
-    ingredients.sort((a, b) => {
+    let sortedIngredients = sortByFoodId(ingredients)
+    // ingredients.sort((a, b) => {
+    //     let fa = a.foodId
+    //     let fb = b.foodId
+    //     if (fa < fb) {
+    //         return -1;
+    //     }
+    //     if (fa > fb) {
+    //         return 1;
+    //     }
+    //     return 0;
+    // })
+
+    let consolidatedIngredients = consolidateIngredients(sortedIngredients)
+    // for(i=0; i < ingredients.length; i++){
+    //     while(ingredients[i+1] !== undefined && 
+    //         (ingredients[i].food.toLowerCase().replace(/-/g, ' ') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
+    //         || (ingredients[i].food.toLowerCase().replace(/-/g, ' ') + 's') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
+    //         || ingredients[i].food.toLowerCase().replace(/-/g, ' ') === (ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') + 's'))){
+    //             ingredients[i].weight = ingredients[i].weight + ingredients[i+1].weight
+    //             ingredients.splice(i+1, 1)
+    //     }
+    // }
+
+    sortedIngredients = sortByFoodName(consolidatedIngredients)
+    // ingredients.sort((a, b) => {
+    //     let fa = a.food
+    //     let fb = b.food
+    //     if (fa < fb) {
+    //         return -1;
+    //     }
+    //     if (fa > fb) {
+    //         return 1;
+    //     }
+    //     return 0;
+    // })
+
+    consolidatedIngredients = consolidateIngredients(sortedIngredients)
+    // for(i=0; i < ingredients.length; i++){
+    //     while(ingredients[i+1] !== undefined && 
+    //         (ingredients[i].food.toLowerCase().replace(/-/g, ' ') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
+    //         || (ingredients[i].food.toLowerCase().replace(/-/g, ' ') + 's') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
+    //         || ingredients[i].food.toLowerCase().replace(/-/g, ' ') === (ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') + 's'))){
+    //             ingredients[i].weight = ingredients[i].weight + ingredients[i+1].weight
+    //             ingredients.splice(i+1, 1)
+    //     }
+    // }
+
+    sortedIngredients = sortByCategory(consolidatedIngredients)
+    // ingredients.sort((a, b) => {
+    //     let fa = !a.category || a.category.toLowerCase()
+    //     let fb = !b.category || b.category.toLowerCase()
+    //     if (fa < fb) {
+    //         return -1;
+    //     }
+    //     if (fa > fb) {
+    //         return 1;
+    //     }
+    //     return 0;  
+    // })
+
+    return sortedIngredients
+}
+
+consolidateIngredients = (ingredients) => {
+    for(i=0; i < ingredients.length; i++){
+        while(ingredients[i+1] !== undefined && 
+            (ingredients[i].food.toLowerCase().replace(/-/g, ' ') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
+            || (ingredients[i].food.toLowerCase().replace(/-/g, ' ') + 's') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
+            || ingredients[i].food.toLowerCase().replace(/-/g, ' ') === (ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') + 's'))){
+                ingredients[i].weight = ingredients[i].weight + ingredients[i+1].weight
+                ingredients.splice(i+1, 1)
+        }
+    }
+
+    return ingredients
+}
+
+sortByFoodId = (item) => {
+    item.sort((a, b) => {
         let fa = a.foodId
         let fb = b.foodId
         if (fa < fb) {
@@ -18,17 +97,11 @@ groceryList = (ingredients) => {
         return 0;
     })
 
-    for(i=0; i < ingredients.length; i++){
-        while(ingredients[i+1] !== undefined && 
-            (ingredients[i].food.toLowerCase().replace(/-/g, ' ') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
-            || (ingredients[i].food.toLowerCase().replace(/-/g, ' ') + 's') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
-            || ingredients[i].food.toLowerCase().replace(/-/g, ' ') === (ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') + 's'))){
-                ingredients[i].weight = ingredients[i].weight + ingredients[i+1].weight
-                ingredients.splice(i+1, 1)
-        }
-    }
+    return item;
+}
 
-    ingredients.sort((a, b) => {
+sortByFoodName = (item) => {
+    item.sort((a, b) => {
         let fa = a.food
         let fb = b.food
         if (fa < fb) {
@@ -40,17 +113,11 @@ groceryList = (ingredients) => {
         return 0;
     })
 
-    for(i=0; i < ingredients.length; i++){
-        while(ingredients[i+1] !== undefined && 
-            (ingredients[i].food.toLowerCase().replace(/-/g, ' ') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
-            || (ingredients[i].food.toLowerCase().replace(/-/g, ' ') + 's') === ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') 
-            || ingredients[i].food.toLowerCase().replace(/-/g, ' ') === (ingredients[i+1].food.toLowerCase().replace(/-/g, ' ') + 's'))){
-                ingredients[i].weight = ingredients[i].weight + ingredients[i+1].weight
-                ingredients.splice(i+1, 1)
-        }
-    }
+    return item;
+}
 
-    ingredients.sort((a, b) => {
+sortByCategory = (item) => {
+    item.sort((a, b) => {
         let fa = !a.category || a.category.toLowerCase()
         let fb = !b.category || b.category.toLowerCase()
         if (fa < fb) {
@@ -62,53 +129,40 @@ groceryList = (ingredients) => {
         return 0;  
     })
 
-    return ingredients
+    return item
 }
 
-getMeals = () => {
-    return JSON.parse(fs.readFileSync(mealsFile));
-}
-
+// fetches the recipe items data
 getIngredients = () => {
     return JSON.parse(fs.readFileSync(ingredientsFile))
 }
 
+// fetches the grocery items data
 getGroceries = () => {
     return JSON.parse(fs.readFileSync(groceriesFile))
 }
 
+// fetches the user items data
 getUserItems = () => {
     return JSON.parse(fs.readFileSync(userItemsFile))
 }
 
-writeMeals = (meals) => {
-    fs.writeFileSync(mealsFile, JSON.stringify([...meals]), err=>console.log(err))
-}
-
+// writes the recipe ingredients data file
 writeIngredients = (ingredients) => {
     fs.writeFileSync(ingredientsFile, JSON.stringify([...ingredients]), err=>console.log(err))
 }
 
+// writes the grocery items data file
 writeGroceries = (groceries) => {
     fs.writeFileSync(groceriesFile, JSON.stringify([...groceries]), err=>console.log(err))
 }
 
+// writes the user items data file
 writeUserItems = (userItems) => {
     fs.writeFileSync(userItemsFile, JSON.stringify([...userItems]), err=>console.log(err))
 }
 
-addNewMeal = (newMeal) => {
-    const meals = getMeals()
-    meals.push(newMeal)
-    writeMeals(meals)
-}
-
-addNewIngredients = (newIngredients) => {
-    const ingredients = getIngredients()
-    ingredients.push(...newIngredients)
-    writeIngredients(ingredients)
-}
-
+// adds the user entered item to the data file
 addNewUserItem = (newUserItem) => {
     // creates new user item object and provides a unique id
     const item = {
@@ -128,6 +182,8 @@ addNewUserItem = (newUserItem) => {
     return item
 }
 
+// finds the clicked item id matching the item passed as an argument and sets its isCompleted value
+// then returns the updated array of items
 setIsCompleted = (itemArray, id) => {
     itemArray.forEach(item => {
         if(item.id===id){
@@ -144,16 +200,12 @@ setIsCompleted = (itemArray, id) => {
 
 module.exports = {
     groceryList,
-    getMeals,
     getIngredients,
     getGroceries,
     getUserItems,
-    writeMeals,
     writeIngredients,
     writeGroceries,
     writeUserItems,
-    addNewMeal,
-    addNewIngredients,
     addNewUserItem,
     setIsCompleted
 }
