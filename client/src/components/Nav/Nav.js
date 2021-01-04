02a1e1fe062712e198b87cb0b411bff376d338f1
David <62397679+MacDavid1989@users.noreply.github.com>
2021-01-04 18:37:23 -0500

    Adjust recipe modal to set top to align with bottom of nav. Refactor scss to reduce unnecessary lines of code.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -11,22 +11,22 @@ function Nav() {
         <>
             <nav className="nav">
                 <div className="nav__container">
-                    <NavLink className={open ? "nav__logo-alt" : "nav__logo"} to="/" onClick={() => {open&&setOpen(!open)}}>
+                    <NavLink className={open ? "nav__logo-alt" : "nav__logo"} to="/" onClick={() => open&&setOpen(!open)}>
                         <img src={logo} alt="grubbery logo"/>
                     </NavLink>
-                    <div className={open ? "nav__burger-alt" : "nav__burger"} onClick={() => {setOpen(!open)}}>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                    <div className={open ? "nav__burger-alt" : "nav__burger"} onClick={() => setOpen(!open)}>
+                        <div className="nav__burger-line"></div>
+                        <div className="nav__burger-line"></div>
+                        <div className="nav__burger-line"></div>
                     </div>
                     <div className={open ? "nav__links-alt" : "nav__links"}>
-                        <NavLink className="nav__links-text" to="/browse" onClick={() => {setOpen(!open)}}>
+                        <NavLink className="nav__links-text" to="/browse" onClick={() => setOpen(!open)}>
                             Browse Recipes
                         </NavLink>
-                        <NavLink className="nav__links-text" to="/menu" onClick={() => {setOpen(!open)}}>
+                        <NavLink className="nav__links-text" to="/menu" onClick={() => setOpen(!open)}>
                             Menu
                         </NavLink>
-                        <NavLink className="nav__links-text" to="/grocery" onClick={() => {setOpen(!open)}}>
+                        <NavLink className="nav__links-text" to="/grocery" onClick={() => setOpen(!open)}>
                             Grocery List
                         </NavLink>
                     </div>
