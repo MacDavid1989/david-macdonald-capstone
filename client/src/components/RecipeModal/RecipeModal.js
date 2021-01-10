766b7d16f8088af8e7548e2c4ede28d668badc16
David <62397679+MacDavid1989@users.noreply.github.com>
2021-01-10 16:35:14 -0500

    Refactor RecipeModal and remove unnecessary code.

--- client/src/components/RecipeModal/RecipeModal.js
+++ client/src/components/RecipeModal/RecipeModal.js
@@ -1,40 +1,42 @@
 import React, { useEffect, useState } from 'react';
 // imported icons
-import close from '../../assets/icons/long-arrow-left.svg'
+import close from '../../assets/icons/long-arrow-left.svg';
 // styling
 import '../../scss/RecipeModal.scss';
 
 // Recipe modal component
-function RecipeModal (props) {
-    const [display, setDisplay] = useState(false)
+function RecipeModal ({ src, resetSrc }) {
 
-    // onClick handler to close modal by calling the resetSrc function and setting display state
-    const closeIframe = () => {
-        props.resetSrc();
-        setDisplay(false)
-    };
-
+    const [display, setDisplay] = useState(false);
+    
     useEffect(()=>{
         // when the sate is updated after mount sets the display to true to keep modal rendered
-        display===false&&props.src&&
-        setDisplay(true)
-    }, [display, props.src]) 
-
+        !display&&src&&setDisplay(true);
+    }, [display, src]); 
+    
+    // onClick handler to close modal by calling the resetSrc function and setting display state
+    const closeIframe = () => {
+        resetSrc();
+        setDisplay(false);
+    };
+    
     return (
         <div className="recipe" style={{ display: display ? "flex" : "none" }}>
             <div className="recipe__frame">
-                <img className="recipe__close" 
+                <img 
+                    className="recipe__close" 
                     src={close} 
                     alt="x symbol" 
                     onClick={closeIframe} 
                 />
-                <iframe className="recipe__source" 
+                <iframe 
+                    className="recipe__source" 
                     title="selected meal recipe website" 
-                    src={props.src}
+                    src={src}
                 ></iframe>
             </div>
         </div>
     );
-}
+};
 
 export default RecipeModal;
