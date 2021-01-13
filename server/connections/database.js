b8f9501512279da6fee399a2cadd4039eed8dad5
David <62397679+MacDavid1989@users.noreply.github.com>
2021-01-13 18:21:33 -0500

    create connections folder and add database.js with initialize to mongodb

---
+++ server/connections/database.js
@@ -0,0 +1,22 @@
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const url = process.env.MONGODB_URL;
+const dbName = "grubbery";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+exports.initialize = () => {
+    return new Promise((resolve, reject) => {
+      client.connect((err) => {
+        if(err){
+            reject(err);
+        }
+        console.log("Connected to database!");
+        db = client.db(dbName);
+      });
+      resolve();
+    });
+}
