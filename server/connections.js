aba65f0b5447e463d33ff5a2ed6ef7fd80530459
David <62397679+MacDavid1989@users.noreply.github.com>
2021-01-13 12:16:33 -0500

    npm i lodash mongoose and logger

--- server/connections.js
+++
@@ -1,9 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://macdavid1989:D3g3N3r8@grubbery.jhshh.mongodb.net/grubbery?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log(collection)
-  client.close();
-});
