92bfd9fa6dd1e1cd25792948c8ab719f16ef899c
David <62397679+MacDavid1989@users.noreply.github.com>
2021-01-13 20:15:35 -0500

    adjust all CRUD operations to utilize db methods

--- server/model/mealListModel.js
+++ server/model/mealListModel.js
@@ -1,8 +1,4 @@
-const fs = require('fs'); // file system module
-const mealsFile = './data/meals.json';
-const ingredientsFile = './data/ingredients.json';
-
-// fetches the meals data
+// fetches all meals from db
 getMeals = () => {
     return new Promise((resolve, reject) => {
         const collection = db.collection("meals");
@@ -12,36 +8,74 @@ getMeals = () => {
           }
           resolve(data);
         });
-      });
+    });
 }
 
-// fetches the recipe items data
-getIngredients = () => {
-    return JSON.parse(fs.readFileSync(ingredientsFile))
+// deletes the meal matching the passed id
+deleteMeals = (meal) => {
+    return new Promise(function (resolve, reject) {
+        const collection = db.collection('meals');
+        collection.deleteOne({ id: meal });
+        console.log('deleted meal')
+        resolve("Deleted meal");
+    });
 }
 
-// writes the meals data file
-writeMeals = (meals) => {
-    fs.writeFileSync(mealsFile, JSON.stringify([...meals]), err=>console.log(err))
+// deletes all ingredients matching the mealId passed
+deleteIngredients = (meal) => {
+    return new Promise(function (resolve, reject) {
+        const collection = db.collection('ingredients');
+        collection.deleteMany({ mealId: meal });
+        console.log('Deleted ingredients')
+        resolve("Deleted ingredients");
+    });
 }
 
-// writes the recipe ingredients data file
-writeIngredients = (ingredients) => {
-    fs.writeFileSync(ingredientsFile, JSON.stringify([...ingredients]), err=>console.log(err))
-}
-
-// pushes the newMeal object to the meal array and overwrites the data file
+// inserts one document(object) to the meals collection of the db
 addNewMeal = (newMeal) => {
-    const meals = getMeals()
-    meals.push(newMeal)
-    writeMeals(meals)
+    return new Promise((resolve, reject) => {
+        const collection = db.collection("meals");
+        collection.insertOne(newMeal, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            console.log("New Meal Added!")
+            resolve("Meal added");
+        });
+    });
 }
 
-// pushes the spread newIngredients array to the ingredients array and overwrites the data file
+// inserts an array of documents(objects) to the ingredients collection of the db
 addNewIngredients = (newIngredients) => {
-    const ingredients = getIngredients()
-    ingredients.push(...newIngredients)
-    writeIngredients(ingredients)
+    return new Promise((resolve, reject) => {
+        const collection = db.collection("ingredients");
+        collection.insertMany(newIngredients, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            console.log("New Ingredients Added!")
+            resolve("Ingredients added");
+        });
+    });
 }
 
-module.exports = { getMeals, getIngredients, writeMeals, writeIngredients, addNewMeal, addNewIngredients }
+module.exports = { 
+    getMeals, 
+    deleteMeals, 
+    deleteIngredients, 
+    addNewMeal, 
+    addNewIngredients 
+}
+
+// // fetches the recipe items data
+// getIngredients = () => {
+//     return new Promise((resolve, reject) => {
+//         const collection = db.collection("ingredients");
+//         collection.find({}).toArray((err, data) => {
+//           if(err){
+//             reject(err);
+//           }
+//           resolve(data);
+//         });
+//       });
+// }
